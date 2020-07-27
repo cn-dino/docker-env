d5e1430e4ee0f20c71c637187bbcdfdf339bec6e
Ian Porteous <porteous@gmail.com>
2020-07-27 16:24:07 +0100

    update with new route

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # for later
 #  docker run --env VAR1=foo alpine:3 env
 
-
-
 import os
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 # get colour from environment, if not default to grey
@@ -14,6 +14,10 @@ print(colour)
 @app.route('/')
 def hello():
     return '<body style="background-color:{}">'.format(colour)
+
+@app.route('/1')
+def one():
+    return render_template("index.html", colour=colour)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
